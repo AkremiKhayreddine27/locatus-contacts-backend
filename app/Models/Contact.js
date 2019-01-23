f9024b97880e188e19fae91fbe1c532cf6b870ed
khayreddine <khayreddine27@gmail.com>
2019-01-23 20:52:14 +0100

    Add before save hook to parse json for contact arrays type

--- app/Models/Contact.js
+++ app/Models/Contact.js
@@ -12,16 +12,6 @@ class Contact extends Model {
      * it to the database.
      */
     this.addHook("beforeSave", async contactInstance => {
-      contactInstance.emails = JSON.stringify(contactInstance.emails);
-      contactInstance.roles = JSON.stringify(contactInstance.roles);
-      contactInstance.rate = JSON.stringify(contactInstance.rate);
-      contactInstance.phoneNumbers = JSON.stringify(
-        contactInstance.phoneNumbers
-      );
-      contactInstance.addresses = JSON.stringify(contactInstance.addresses);
-    });
-
-    this.addHook("beforeUpdate", async contactInstance => {
       if (contactInstance.dirty.emails) {
         contactInstance.emails = JSON.stringify(contactInstance.emails);
       }
