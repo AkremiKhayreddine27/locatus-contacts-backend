0e8fce8ff31b88fed9e4532dfbdacd566927937c
khayreddine <khayreddine27@gmail.com>
2019-01-23 16:59:11 +0100

    Add before save hook to parse json for contact arrays type

--- app/Models/Contact.js
+++ app/Models/Contact.js
@@ -20,6 +20,26 @@ class Contact extends Model {
       );
       contactInstance.addresses = JSON.stringify(contactInstance.addresses);
     });
+
+    this.addHook("beforeUpdate", async contactInstance => {
+      if (contactInstance.dirty.emails) {
+        contactInstance.emails = JSON.stringify(contactInstance.emails);
+      }
+      if (contactInstance.dirty.roles) {
+        contactInstance.roles = JSON.stringify(contactInstance.roles);
+      }
+      if (contactInstance.dirty.rate) {
+        contactInstance.rate = JSON.stringify(contactInstance.rate);
+      }
+      if (contactInstance.dirty.phoneNumbers) {
+        contactInstance.phoneNumbers = JSON.stringify(
+          contactInstance.phoneNumbers
+        );
+      }
+      if (contactInstance.dirty.addresses) {
+        contactInstance.addresses = JSON.stringify(contactInstance.addresses);
+      }
+    });
   }
 
   static get createdAtColumn() {
